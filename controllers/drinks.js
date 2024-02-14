46627d141f7da7bd15e0bdb643236a9f68f3dba8
Mykola1612 <fermer2007pavlovich@gmail.com>
2024-02-14 20:35:38 +0100

    Update drinks.js

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -4,7 +4,10 @@ const { ctrlWrapper } = require("../helpers");
 const addDrink = async (req, res, next) => {
   //   const { _id: owner } = req.user;
   const result = await UserDrinksDB.create({ ...req.body });
-  res.json(result);
+  const updatedResult = await UserDrinksDB.findById(result._id).select(
+    "-createdAt -updatedAt"
+  );
+  res.status(201).json(updatedResult);
 };
 
 module.exports = {
