29a0a2972c6baa59aba9e547608c42be628f1293
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-02-16 00:41:13 +0200

    drinks main page

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/drinks");
 
 const router = express.Router();
 const { isAuthenticated } = require("../../middlewares");
-
+// isAuthenticated добавлю позже
 router.get("/", ctrl.listDrink);
 
 router.post("/own/add", isAuthenticated, ctrl.addDrink);
