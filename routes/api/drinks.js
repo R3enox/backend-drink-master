bc859df63ce2fd56ad4f3b41f78659198a61dbba
Vadym Bobyrenko <wadimcka@gmail.com>
2024-02-16 17:22:35 +0100

    add controller getMyDrinks , rout own, fix middelewar isValidId

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const ctrl = require("../../controllers/drinks");
 const { isAuthenticated } = require("../../middlewares");
+
+const ctrl = require("../../controllers/drinks");
 
 const router = express.Router();
 
-router.post("/own/add", ctrl.addDrink);
+router.post("/own/add", isAuthenticated, ctrl.addDrink);
 
 router.get("/own", isAuthenticated, ctrl.getMyDrinks);
 
