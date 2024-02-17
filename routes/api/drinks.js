0f9e131b8396501289beb75af6f3c4335554e8ec
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2024-02-17 04:26:03 +0200

    drinks search

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -1,12 +1,10 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/drinks");
-
 const router = express.Router();
 const { isAuthenticated } = require("../../middlewares");
-// isAuthenticated добавлю позже
-router.get("/", ctrl.listDrink);
-
+router.get("/", isAuthenticated, ctrl.listDrinks);
+router.get("/search", isAuthenticated, ctrl.searchDrinks);
 router.post("/own/add", isAuthenticated, ctrl.addDrink);
 
 module.exports = router;
