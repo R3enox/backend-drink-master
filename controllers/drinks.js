1bf9eec445989264ac0851910e01cd0314d93c20
Mykola1612 <fermer2007pavlovich@gmail.com>
2024-02-17 13:48:18 +0100

    Summary

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -1,13 +1,30 @@
+// const fs = require("fs/promises");
+// const path = require("path");
+const Jimp = require("jimp");
 const { UserDrinksDB } = require("../models/drinks");
 const { ctrlWrapper, userAge, HttpError } = require("../helpers");
 
+// const avatarDir = path.join(__dirname, "../", "public", "avatars");
+
 const addDrink = async (req, res, next) => {
-  const { _id: owner, birthdate } = req.user;
+  // const { path: tempUpload, originalname } = req.file;
+  const { _id: owner, dateOfBirth } = req.user;
   const { alcoholic } = req.body;
-  const age = userAge(birthdate);
+  const age = userAge(dateOfBirth);
   if (alcoholic === "Alcoholic" && age < 18) {
     throw HttpError(400);
   }
+
+  // const image = await Jimp.read(tempUpload);
+  // await image.resize(250, 250);
+
+  // const fileName = `${_id}_${originalname}`;
+  // console.log(fileName);
+  // const resultUpdateAvatar = path.join(avatarDir, fileName);
+
+  // await image.writeAsync(resultUpdateAvatar);
+  // await fs.unlink(tempUpload);
+  // const avatarURL = path.join("avatars", fileName);
 
   const result = await UserDrinksDB.create({ ...req.body, owner });
   const updatedResult = await UserDrinksDB.findById(result._id).select(
