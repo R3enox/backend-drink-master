ee59f836e1f0c49b8a672ef8b0cd8f5aab8f6c0c
Sergey Lutsenko <lytsenko.89@gmail.com>
2024-02-18 12:49:39 +0200

    favorite changes

--- routes/api/drink.js
+++ routes/api/drink.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const ctrl = require("../../controllers/drinkController");
 const { isAuthenticated } = require("../../middlewares");
 
-router.get("/:drinkId", isAuthenticated, ctrl.getContactById);
+router.get("/:drinkId", isAuthenticated, ctrl.getDrinkById);
 
 module.exports = router;
