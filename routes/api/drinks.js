0a1cfcf22b362ded3bda9843803880222080e4a2
Vadym Bobyrenko <wadimcka@gmail.com>
2024-02-19 17:27:43 +0100

    cange delete  rout to  /own/remove/:id

--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -2,20 +2,19 @@ const express = require("express");
 
 const ctrl = require("../../controllers/drinks");
 
-const checkConfirmation = require("../../middlewares/checkConfirmation");
+// const checkConfirmation = require("../../middlewares/checkConfirmation");
 
 const router = express.Router();
 const { isAuthenticated } = require("../../middlewares");
+
 router.get("/", isAuthenticated, ctrl.listDrinks);
 router.get("/search", isAuthenticated, ctrl.searchDrinks);
 router.post("/own/add", isAuthenticated, ctrl.addDrink);
-
-router.get("/own/:id", isAuthenticated, ctrl.getMyDrinks);
-
+router.get("/own", isAuthenticated, ctrl.getMyDrinks);
 router.delete(
   "/own/remove/:id",
   isAuthenticated,
-  checkConfirmation,
+  // checkConfirmation,
   ctrl.deleteMyDrink
 );
 
